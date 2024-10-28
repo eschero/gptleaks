3ee7808cd6c5b227f1876f5e505f13022443ac41
eschero <70647757+eschero@users.noreply.github.com>
2024-10-28 16:34:33 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 // Percorso: gptleaks-main/app/page.tsx
-"use client";
+'use client';
 import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer';
 import { allBlogs } from 'contentlayer/generated';
 import Main from './Main';
 import AsciiAnimation from '../components/AsciiAnimation';
 
-export default async function Page() {
+export default function Page() {
   const sortedPosts = sortPosts(allBlogs);
   const posts = allCoreContent(sortedPosts);
 
