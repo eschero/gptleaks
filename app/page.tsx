b4a858aa09ac1a3cf7571da9abbe4367c9d8dbb3
eschero <70647757+eschero@users.noreply.github.com>
2024-10-30 15:49:11 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,14 +14,14 @@ export default function Page() {
       {/* Contenitore dell'animazione */}
       <div
         id="container"
-        className="relative flex h-[85vh] w-full items-center justify-center overflow-x-hidden"
+        className="relative flex h-[40vh] w-full items-center justify-center overflow-x-hidden"
         style={{
           background: 'linear-gradient(180deg, #030712, #030712)',
         }}
       >
         <div
           id="canvasContainer"
-          className="absolute inset-[0.5rem] max-h-full w-[calc(100%-2rem)] overflow-hidden rounded-[1.5rem] border-2 border-[#181818] bg-[#30712] shadow-lg sm:inset-[1.5rem] sm:max-h-[85vh] sm:w-[calc(100%-3rem)] sm:rounded-[2rem] lg:inset-[3rem] lg:max-h-[85vh] lg:w-[calc(100%-6rem)] lg:rounded-[3rem]"
+          className="absolute inset-[0.5rem] max-h-full w-[calc(100%-2rem)] overflow-hidden rounded-[0.75rem] border-2 border-[#181818] bg-[#30712] shadow-lg sm:inset-[1.5rem] sm:max-h-[85vh] sm:w-[calc(100%-3rem)] sm:rounded-[2rem] lg:inset-[3rem] lg:max-h-[85vh] lg:w-[calc(100%-6rem)] lg:rounded-[3rem]"
         >
           <canvas id="myCanvas" className="w-full"></canvas>
           <AsciiAnimation />
