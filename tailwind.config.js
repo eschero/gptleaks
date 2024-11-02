5367ab0b93f59539ab822927a23431c52e2c275a
eschero <70647757+eschero@users.noreply.github.com>
2024-11-02 12:26:13 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,8 +25,8 @@ module.exports = {
         sans: ['var(--font-space-grotesk)', ...fontFamily.sans],
       },
       colors: {
-        primary: colors.pink,
-        gray: colors.gray,
+        primary: colors.blue,
+        gray: colors.zinc,
       },
       zIndex: {
         60: '60',
